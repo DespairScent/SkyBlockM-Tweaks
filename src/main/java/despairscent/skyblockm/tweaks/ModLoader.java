d32be9025b48198c545db499afa43500e362de19
DespairScent <74109497+DespairScent@users.noreply.github.com>
2025-06-03 09:15:30 +0300

    Убраны остатки от оптимизации FPS
    А зачем оно там... Да и файл локализации теперь используется для "просмотра возможностей мода", и оставлять там оптимизации FPS будет обманом для читающих.
    
    В классе конфига оставлено на случай возможного возврата, что бы настроенные параметры сохранились.

--- src/main/java/despairscent/skyblockm/tweaks/ModLoader.java
+++ src/main/java/despairscent/skyblockm/tweaks/ModLoader.java
@@ -13,25 +13,6 @@ public class ModLoader implements ClientModInitializer {
         config = Config.load();
         config.save();
 
-        // KeyBinding keybindingSwitchOptimize = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-        //         "skyblockm-tweaks.keys.optimize",
-        //         InputUtil.Type.KEYSYM,
-        //         InputUtil.UNKNOWN_KEY.getCode(),
-        //         "skyblockm-tweaks.keys"
-        // ));
-        // ClientTickEvents.END_CLIENT_TICK.register(minecraftClient -> {
-        //     while (keybindingSwitchOptimize.wasPressed()) {
-        //         if (client.world == null || client.player == null) {
-        //             return;
-        //         }
-        //         config.modules.fpsOptimize = !config.modules.fpsOptimize;
-        //         client.inGameHud.getChatHud().addMessage(i18n("message.optimizeSwitch").append(
-        //                 config.modules.fpsOptimize ?
-        //                         Text.literal("on").styled(style -> style.withColor(Formatting.GREEN)) :
-        //                         Text.literal("off").styled(style -> style.withColor(Formatting.RED))));
-        //     }
-        // });
-
         CompactGenomeModule.init();
     }
 
