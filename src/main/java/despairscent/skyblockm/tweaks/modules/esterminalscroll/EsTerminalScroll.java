092b52ee0bd06ab3ff593d3ba40cb2ca229a0467
DespairScent <74109497+DespairScent@users.noreply.github.com>
2025-06-06 11:10:25 +0300

    Добавлена возможность прокрутки в окне создающихся предметов и ES-процессора создания

--- src/main/java/despairscent/skyblockm/tweaks/modules/esterminalscroll/EsTerminalScroll.java
+++ src/main/java/despairscent/skyblockm/tweaks/modules/esterminalscroll/EsTerminalScroll.java
@@ -43,7 +43,9 @@ public class EsTerminalScroll {
         }
 
         int slot;
-        if (ModUtils.testCustomScreen(screen, "electric_storage:interfaces", "\u1000")) {
+        if (ModUtils.testCustomScreen(screen, "electric_storage:interfaces", "\u1000") ||
+                ModUtils.testCustomScreen(screen, "electric_storage:interfaces", "\u1030") ||
+                ModUtils.testCustomScreen(screen, "electric_storage:interfaces", "\u2000")) {
             slot = up ? 8 : 35;
         } else if (ModUtils.testCustomScreen(screen, "electric_storage:interfaces", "\u1010")) {
             slot = up ? 11 : 38;
